3f6cac0fac43375d2fd5b190403882710f6b1ebf
altrios <famartinez.ti@gmail.com>
2023-12-18 11:48:57 +0100

    v 0.0.0.5 remove some comments

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -5,7 +5,6 @@ const app = express();
 const port = 3000;
 const cors = require('cors');
 app.use(express.json());
-// Configurar dotenv para cargar variables de entorno
 require('dotenv').config();
 
 app.use(cors());
@@ -15,19 +14,16 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 const db = mongoose.connection;
 db.once('open', () => {
-  db.useDb('Pokeapi'); // Cambia a la base de datos "Pokeapi"
+  db.useDb('Pokeapi'); 
   console.log(`Conexión exitosa a la base de datos: ${db.name}`);
-  // Resto de tu código
 });
 
 
-// Rutas y lógica de tu aplicación
 app.get('/', (req, res) => {
   res.send('¡Hola, mundo!');
 });
 app.use('/api/v1/pokelist', require('./app/route/pokelist.route'));
 
-// Iniciar el servidor
 app.listen(port, () => {
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
