955ddb3a6ce32c3443e844ab30501f241ca72dbc
Fernando <famartinez.ti@gmail.com>
2026-01-21 20:53:19 +0100

    style: :sparkles:

--- Front-end/pokeapi/src/App.jsx
+++ Front-end/pokeapi/src/App.jsx
@@ -1,57 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-  Link
-} from 'react-router-dom';
-import Pokelist from './components/pokelist.component';
+  Link,
+} from "react-router-dom";
+import Pokelist from "./components/pokelist.component";
 import usePokemonContext from "./hooks/usePokemonContext";
-import PokeCardDetail from './components/pokeCardDetail.component';
-import Login from './components/login.component';
-import Register from './components/register.component';
+import PokeCardDetail from "./components/pokeCardDetail.component";
+import Login from "./components/login.component";
+import Register from "./components/register.component";
 
 function App() {
   const { pokemonDetail, isLoading } = usePokemonContext() || {};
-  const [login, setLogin] = useState(false)
-  const [userbar, setUsebar] = useState(false)
+  const [login, setLogin] = useState(false);
+  const [userbar, setUsebar] = useState(false);
 
   // Check if "token" is present in session storage
   const logout = () => {
-    sessionStorage.removeItem('token');
-    sessionStorage.removeItem('username');
-    setUsebar(false)
-  }
+    sessionStorage.removeItem("token");
+    sessionStorage.removeItem("username");
+    setUsebar(false);
+  };
 
   useEffect(() => {
-    const hasToken = !!sessionStorage.getItem('token');
+    const hasToken = !!sessionStorage.getItem("token");
     if (hasToken) {
-      setUsebar(true)
-
+      setUsebar(true);
+      console.log("hagamos un cambio ");
     }
-
-  }, [])
+  }, []);
   // console.log = function() {};
   // console.error = function() {};
-  return (  
+  return (
     <Router>
-      {userbar &&
+      {userbar && (
         <nav>
           <ul>
-          <li>
-              <Link to="/" > Pokemon List</Link>
+            <li>
+              <Link to="/"> Pokemon List</Link>
             </li>
-            <li style={{float:'right'}}>
-              <Link to="/login" onClick={logout}>Logout</Link>
+            <li style={{ float: "right" }}>
+              <Link to="/login" onClick={logout}>
+                Logout
+              </Link>
             </li>
-            
           </ul>
         </nav>
-      }
+      )}
       <section className="section-container justify-center items-center">
         <main>
-          <div className='content justify-center items-center'>
+          <div className="content justify-center items-center">
             <Routes>
               <Route path="/" element={<Pokelist />} />
               <Route path="/poke/:id" element={<PokeCardDetail />} />
